f71f8027e1238a32b29149de594b61449db1c449
Alex Folland <lexlexlex@gmail.com>
2021-05-20 04:04:45 -0700

    remove need for perl by using winepath; I'm still requesting help with making this work with spaces in paths.

--- generatetortoiseproccommand.sh
+++ generatetortoiseproccommand.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 IFS=$'\n'
-svnpaths=''
-for svnpath in ${@:2} ; do
-	#echo "arg = $svnpath"
-	svnpaths+="Z:$svnpath*"
+svnpaths="$(winepath --windows "${2}")"
+for additionalsvnpath in ${@:3} ; do
+	svnpaths+="*$(winepath --windows "${additionalsvnpath}")"
 done
-IFS=$' '
-svnpaths=`perl -pe 's/\//\\\\\\\\/g' <<< $svnpaths`
-#echo "svnpaths = $svnpaths"
-svnpaths=`perl -pe "s/^(.*)\*$/\\$1/g" <<< $svnpaths`
-#echo "svnpaths = $svnpaths"
-#svnpaths=`perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$svnpaths"`
-#svnpaths=`perl -pe "s/ /\\\\\\\\ /g" <<< $svnpaths`
-#echo "svnpaths = $svnpaths"
-#echo "running this:"
-bashcommand="wine TortoiseProc \"/command:$1\" \"/path:\"${svnpaths}\"\" &> /dev/null"
-bash -v -c "$bashcommand"
-exit 0
+wine TortoiseProc.exe "/command:${1}" "/path:${svnpaths}" &> /dev/null
+
+#set -x
+#{ set +x; } 2> /dev/null
+#svnpaths="$(perl -pe "s/(?<!:)\\\\/\\\\\\\\/g" <<< $svnpaths)"
+#svnpaths="$(perl -pe "s/ /\\\\ /g" <<< $svnpaths)"
+#command=$'TortoiseProc "/command:$1" "\"/path:${svnpaths}\"" &> /dev/null'
+#exit 0
+
+#function tort { local WPATH=`winepath --windows $2`; wine "TortoiseProc.exe" "/command:${1}" "/path:${WPATH}"; } export -f tort 
